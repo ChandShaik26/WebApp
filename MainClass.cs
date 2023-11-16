01a764f491be2aac6976fb2abe55c0cee989c731
ChandShaik26 <schand2226@gmail.com>
2023-11-16 15:04:49 +0530

    Revert "Keep single console message"
    This reverts commit ef64baa6c8c5d2e403d0e7a1d4c964f49e395bd0.

--- MainClass.cs
+++ MainClass.cs
@@ -1,5 +1,5 @@
-﻿Console.WriteLine("Hello Chand! This is the practical part of Github. This has been done in GitHub");
-
+﻿Console.WriteLine("Hello Chand! This is the pratical part of Github. This has been done in github");
+;
 
 
 
