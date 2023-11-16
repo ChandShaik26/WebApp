369557c38ffee1f644d1922e87f6b1d283e58c81
ChandShaik26 <schand2226@gmail.com>
2023-11-16 15:17:28 +0530

    Created myMethod

--- MainClass.cs
+++ MainClass.cs
@@ -1,5 +1,7 @@
-﻿Console.WriteLine("Hello Chand! This is the pratical part of Github. This has been done in github");
-;
+﻿myMethod();
 
-
-
+ 
+void myMethod()
+{
+    Console.WriteLine("Hello Chand!");
+}
