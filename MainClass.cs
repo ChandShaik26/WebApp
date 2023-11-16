8b7264e6407c736423a0a6e546bfbaea31f851e9
ChandShaik26 <146055785+ChandShaik26@users.noreply.github.com>
2023-11-16 14:51:29 +0530

    Update MainClass.cs
    Keeping single console

--- MainClass.cs
+++ MainClass.cs
@@ -1,11 +1,5 @@
 ﻿Console.WriteLine("Hello Chand! This is the pratical part of Github. This has been done in github");
-Console.WriteLine("Hello Chand! This is the pratical part of Github. This has been done in github");
-
-Console.WriteLine("Hello Chand! This is the pratical part of Github. This has been done in github");
-Console.WriteLine("Hello Chand! This is the pratical part of Github. This has been done in github");
-
-Console.WriteLine("Hello Chand! This is the pratical part of Github. This has been done in github");
-Console.WriteLine("Hello Chand! This is the pratical part of Github. This has been done in github");
+;
 
 
 
